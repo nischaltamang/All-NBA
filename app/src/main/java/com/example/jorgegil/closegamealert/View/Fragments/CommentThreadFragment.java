d10ae34b8f6fb5c37df6440de1d9082346e1dff1
Jorge Gil <jorgegilcavazos@gmail.com>
2016-09-29 21:33:08 -0500

    Show/Hide progress spinner. Close #16

--- app/src/main/java/com/example/jorgegil/closegamealert/View/Fragments/CommentThreadFragment.java
+++ app/src/main/java/com/example/jorgegil/closegamealert/View/Fragments/CommentThreadFragment.java
@@ -1,35 +1,26 @@
 package com.example.jorgegil.closegamealert.View.Fragments;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 
 import com.example.jorgegil.closegamealert.Adapter.CommentAdapter;
-import com.example.jorgegil.closegamealert.General.Comment;
 import com.example.jorgegil.closegamealert.R;
-import com.example.jorgegil.closegamealert.General.TeamName;
 import com.example.jorgegil.closegamealert.Utils.RedditAuthentication;
 
 import net.dean.jraw.http.SubmissionRequest;
 import net.dean.jraw.models.CommentNode;
 import net.dean.jraw.models.CommentSort;
-import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Submission;
 
 import java.util.ArrayList;
@@ -46,15 +37,15 @@ public class CommentThreadFragment extends Fragment {
 
     private String homeTeam;
     private String awayTeam;
-    private int threadType;
+    private String mThreadId;
+    private int mThreadType;
 
-    Context context;
-
-    boolean foundThread = false;
+    Context mContext;
 
     RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLayoutManager;
     CommentAdapter mCommentAdapter;
+    ProgressBar mProgressBar;
 
 
     @Override
@@ -63,11 +54,11 @@ public class CommentThreadFragment extends Fragment {
         if (getArguments() != null) {
             homeTeam = getArguments().getString(HOME_TEAM_KEY);
             awayTeam = getArguments().getString(AWAY_TEAM_KEY);
-            threadType = getArguments().getInt(THREAD_TYPE_KEY);
+            mThreadType = getArguments().getInt(THREAD_TYPE_KEY);
         }
         setHasOptionsMenu(true);
 
-        context = getActivity();
+        mContext = getActivity();
     }
 
     @Override
@@ -75,8 +66,9 @@ public class CommentThreadFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_comment_thread, container, false);
 
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.comment_thread_rv);
-        mLayoutManager = new LinearLayoutManager(context);
+        mLayoutManager = new LinearLayoutManager(mContext);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -85,18 +77,22 @@ public class CommentThreadFragment extends Fragment {
             ViewCompat.setNestedScrollingEnabled(mRecyclerView, false);
         }
 
-        new FetchSubmission("54s74i").execute();
-
+        mThreadId = "54s74i";
+        fetchComments(mThreadId);
         return view;
     }
 
-    private void getComments(Submission submission) {
+    private void fetchComments(String submissionId) {
+        new FetchSubmission(submissionId).execute();
+    }
+
+    private void loadComments(Submission submission) {
         Iterable<CommentNode> iterable = submission.getComments().walkTree();
         List<CommentNode> commentNodes = new ArrayList<>();
         for (CommentNode node : iterable) {
             commentNodes.add(node);
         }
-        mCommentAdapter = new CommentAdapter(context, commentNodes);
+        mCommentAdapter = new CommentAdapter(mContext, commentNodes);
         mRecyclerView.setAdapter(mCommentAdapter);
     }
 
@@ -104,7 +100,7 @@ public class CommentThreadFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                //  Do something
+                fetchComments(mThreadId);
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -118,9 +114,15 @@ public class CommentThreadFragment extends Fragment {
         }
 
         @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+        }
+
+        @Override
         protected Submission doInBackground(Void... params) {
             SubmissionRequest.Builder builder = new SubmissionRequest.Builder(mThreadId);
-            switch (threadType) {
+            switch (mThreadType) {
                 case LIVE_THREAD:
                     builder.sort(CommentSort.NEW);
                     break;
@@ -137,8 +139,10 @@ public class CommentThreadFragment extends Fragment {
 
         @Override
         protected void onPostExecute(Submission submission) {
+            mProgressBar.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
             if (submission != null) {
-                getComments(submission);
+                loadComments(submission);
             }
         }
     }
